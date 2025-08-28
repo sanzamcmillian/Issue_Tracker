a4276e19347070b16269386c33b14b04c5216f47
sanzamcmillian <g18s2658@ru.ac.za>
2025-08-28 22:10:50 +0200

    Update main.py
    adjusting the port for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .api.endpoints import issues
@@ -17,3 +19,8 @@ app.add_middleware(
 )
 
 app.include_router(issues.router)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app,", host="0.0.0.0", port=port, reload=False)
